473a90781f1d12787144e02d4aa2a6ac37e533d4
adamb <adam.boustta@student.uantwerpen.be>
2024-03-16 16:36:21 +0100

    add device and job classes

---
+++ src/Device.h
@@ -0,0 +1,11 @@
+
+#ifndef PSE_PROJECT_DEVICE_H
+#define PSE_PROJECT_DEVICE_H
+
+
+class Device {
+
+};
+
+
+#endif //PSE_PROJECT_DEVICE_H
---
+++ src/Job.h
@@ -0,0 +1,11 @@
+
+#ifndef PSE_PROJECT_JOB_H
+#define PSE_PROJECT_JOB_H
+
+
+class Job {
+
+};
+
+
+#endif //PSE_PROJECT_JOB_H
