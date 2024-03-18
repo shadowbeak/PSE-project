e4ecb5afa7ba990c56b0880a0960facb14d5b4b0
gengisvdf <158093842+gengisvdf@users.noreply.github.com>
2024-03-18 09:44:24 +0000

    Update PrintSystemUtils.h

--- src/PrintSystemUtils.h
+++ src/PrintSystemUtils.h
@@ -14,21 +14,46 @@
 #include <cctype>
 #include "Device.h"
 
-
+/**
+ * \brief Controleert of een directory bestaat
+ * @param dirname De naam van de directory om te controleren
+ * @return true als de directory bestaat
+ */
 bool DirectoryExists(const std::string dirname);
 
+/**
+ * \brief Controleert of een bestand bestaat
+ * @param dirname De naam van het bestand om te controleren
+ * @return true als het bestand bestaat
+ */
 bool FileExists(const std::string dirname);
 
+/**
+ * \brief Controleert of een bestand leeg is
+ * @param filename De naam van het bestand om te controleren
+ * @return true als het bestand leeg is
+ */
 bool FileIsEmpty(const std::string filename);
 
+/**
+ * \brief Vergelijkt twee bestanden
+ * @param leftFileName De naam van het linkerbestand
+ * @param rightFileName De naam van het rechterbestand
+ * @return true als de bestanden identiek zijn
+ */
 bool FileCompare(const std::string leftFileName, const std::string rightFileName);
 
-bool isInt(const std::string& str);
+/**
+ * \brief Controleert of een string een getal is
+ * @param s De string om te controleren
+ * @return true als de string een getal is
+ */
+bool is_number(const std::string& s);
 
-bool isNotInDevice(Job *job, std::deque<Job*>& jobs);
-
-bool isNegative(int value);
-
-std::string constructFilename(const std::string& storageDirectory, const std::string& reportExtension);
-
-#endif //PSE_PROJECT_PRINTSYSTEMUTILS_H
+/**
+ * \brief Genereert een bestandsnaam
+ * @param prefix Het voorvoegsel voor de bestandsnaam
+ * @param extension De bestandsextensie
+ * @return De gegenereerde bestandsnaam
+ */
+std::string GenerateFileName(const std::string &prefix, const std::string &extension);
