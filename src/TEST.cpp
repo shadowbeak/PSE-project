6cb8417dc21f35def8db9a7ad682d373e73baf66
adamb <adam.boustta@student.uantwerpen.be>
2024-03-20 18:05:47 +0100

    eggeg

--- src/TEST.cpp
+++ src/TEST.cpp
@@ -18,6 +18,7 @@ protected:
 
 //Jobs:
 TEST_F(TestPrintSystem, NoPageCount){
+
 }
 
 TEST_F(TestPrintSystem, NoJobNumber){
@@ -28,10 +29,11 @@ TEST_F(TestPrintSystem, NoUserName){
 
 
 //Devices:
-TEST_F(TestPrintSystem, NoSpeed){
+TEST_F(TestPrintSystem, NoSpeed) {
 }
 
 TEST_F(TestPrintSystem, NoEmission){
+
 }
 
 TEST_F(TestPrintSystem, NoName){
@@ -40,13 +42,7 @@ TEST_F(TestPrintSystem, NoName){
 
 //Reports:
 TEST_F(TestPrintSystem, checkPrintReport){
-    system->Readfile("xmlTests/ManualTest");
-    system->getDevices()[0]->addJob(system->getJobs()[0]);
-    system->getDevices()[1]->addJob(system->getJobs()[1]);
-    system->getDevices()[2]->addJob(system->getJobs()[2]);
-    std::string fileReport = system->printReport();
-    std::string fileToCompare = "reportTest/reportTest1";
-    EXPECT_TRUE(FileCompare(fileReport, fileToCompare));
+
 
 }
 
